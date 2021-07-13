775b87eecbbae4f0f25ad9ddf2e224d2b6847f77
KKoczur <karol@bluepoint-IT.pl>
2021-07-13 07:20:19 +0200

    Added: NegativeChecker app.

---
+++ NegativeChecker/NegativeChecker/NegativeChecker/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Dynamic;
+
+namespace NegativeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set number");
+            string userInput = Console.ReadLine();
+
+            bool isNegative = TryParseToNegativeInt(userInput, out int? negaiveNumberValue);
+
+            if (isNegative)
+            {
+                Console.WriteLine($"Number {negaiveNumberValue} is negtive.");
+            }
+            else
+            {
+                Console.WriteLine($"Number is positive");
+            }
+
+
+        }
+
+        static bool TryParseToNegativeInt(string userInput, out int? negativeInt)
+        {
+            negativeInt = null;
+
+            if (int.TryParse(userInput, out int number))
+            {
+                if (number < 0)
+                {
+                    negativeInt = number;
+                    return true;
+                }
+                else
+                {
+
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Can't parse user input {userInput} to number");
+                return false;
+            }
+
+        }
+    }
+}
