9e76535b53a85d0484269acf2efd18102de9589a
KKoczur <karol@bluepoint-IT.pl>
2021-08-05 07:59:36 +0200

    Refactoring. ListViewColumnSorter

--- StatlookLogViewer/UplookLogViewer/ListViewColumnSorter.cs
+++ StatlookLogViewer/UplookLogViewer/ListViewColumnSorter.cs
@@ -11,7 +11,7 @@ namespace StatlookLogViewer
         /// <summary>
         /// Case insensitive comparer object
         /// </summary>
-        private CaseInsensitiveComparer ObjectCompare;
+        private readonly CaseInsensitiveComparer _caseInsensitiveComparer;
 
 		/// <summary>
 		/// Class constructor.  Initializes various elements
@@ -25,7 +25,7 @@ namespace StatlookLogViewer
 			Order = SortOrder.None;
 
 			// Initialize the CaseInsensitiveComparer object
-			ObjectCompare = new CaseInsensitiveComparer();
+			_caseInsensitiveComparer = new CaseInsensitiveComparer();
 		}
 
 		/// <summary>
@@ -44,25 +44,30 @@ namespace StatlookLogViewer
 			listviewY = (ListViewItem)y;
 
 			// Compare the two items
-			compareResult = ObjectCompare.Compare(listviewX.SubItems[SortColumn].Text, listviewY.SubItems[SortColumn].Text);
+			compareResult = _caseInsensitiveComparer.Compare(listviewX.SubItems[SortColumn].Text, listviewY.SubItems[SortColumn].Text);
 
-			// Calculate correct return value based on object comparison
-			if (Order == SortOrder.Ascending)
-			{
-				// Ascending sort is selected, return normal result of compare operation
-				return compareResult;
-			}
-			else if (Order == SortOrder.Descending)
-			{
-				// Descending sort is selected, return negative result of compare operation
-				return (-compareResult);
-			}
-			else
-			{
-				// Return '0' to indicate they are equal
-				return 0;
-			}
-		}
+            // Calculate correct return value based on object comparison
+            switch (Order)
+            {
+                case SortOrder.Ascending:
+                    {
+                        // Ascending sort is selected, return normal result of compare operation
+                        return compareResult;
+                    }
+
+                case SortOrder.Descending:
+                    {
+                        // Descending sort is selected, return negative result of compare operation
+                        return -compareResult;
+                    }
+
+                default:
+                    {
+                        // Return '0' to indicate they are equal
+                        return 0;
+                    }
+            }
+        }
 
         /// <summary>
         /// Gets or sets the number of the column to which to apply the sorting operation (Defaults to '0').
@@ -75,5 +80,5 @@ namespace StatlookLogViewer
         public SortOrder Order { set; get; }
 
     }
-	}
+}
 
