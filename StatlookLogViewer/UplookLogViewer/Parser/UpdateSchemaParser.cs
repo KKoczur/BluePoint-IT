21f648fa78c2790fb879700abc6f1c00e1678c8e
KKoczur <karol@bluepoint-IT.pl>
2021-10-15 20:15:07 +0200

    Added UpdateSchemaParser

---
+++ StatlookLogViewer/UplookLogViewer/Parser/UpdateSchemaParser.cs
@@ -0,0 +1,32 @@
+﻿using StatlookLogViewer.Model.Pattern;
+using System.Collections.Generic;
+
+namespace StatlookLogViewer.Parser
+{
+    internal class UpdateSchemaParser : ILogParser
+    {
+        public string UniqueLogKey { get; set; } = "Update Database Schema";
+
+        public string StartLogGroupEntry { get; set; } = "startTime";
+
+        public string EndLogGroupEntry { get; set; } = "--------------------------------------------------------------------------------";
+
+        public LogErrorPattern[] GetListOfErrors()
+        {
+            return new List<LogErrorPattern>()
+            {
+            }.ToArray();
+        }
+
+        public IEnumerable<LogPattern> GetLogPatterns()
+        {
+            return new List<LogPattern>()
+            {
+                new LogPattern( "startTime", "	Start time:", true),
+                new LogPattern( "duration", "	Duration:", true),
+                new LogPattern( "conditionalExpressionResult", "	Conditional expression result:", true),
+                new LogPattern( "status", "	Status:", true)
+             }.ToArray();
+        }
+    }
+}
